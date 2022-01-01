6a611a907cefc614e600a754772e0375dbc9ea69
HyeonGeunY <hgyoon0928@gmail.com>
2022-01-01 13:38:04 +0000

    22.01.01

--- LABs/lab1/text_recognizer/lit_models/base.py
+++ LABs/lab1/text_recognizer/lit_models/base.py
@@ -27,4 +27,104 @@ class Accuracy(pl.metrics.Accuracy):
 
 
 class BaseLitModel(pl.lightningModule):
-    """ """
+    """
+    파이토치 모듈로 initialized 되는 일반벅인 pytorch-lightning class
+    """
+
+    def __init__(self, model, args: argparse.Namespace=None):
+        super().__init__()
+        self.model = model # 모델을 받음
+        self.args = vars(args) if args is not None else {}
+
+        optimizer = self.args.get("optimizer", OPTIMIZER)
+        # getattr(object, name[, default]), object에서 "name" attribute를 가져온다. 값이 없을 경우 default값을 가져온다.
+        self.optimizer_class = getattr(torch.optim, optimizer) # torch.optim에서 optimzer attribute을 가져온다.
+
+        self.lr = self.args.get("lr", LR)
+
+        loss = self.args.get("loss", LOSS)
+        if loss not in ("ctc", "transformer"):
+            self.loss_fn = getattr(torch.nn.functional, loss)
+        
+        self.one_cycle_max_lr = self.args.get("one_cycle_max_lr", None)
+        self.one_cycle_total_steps = self.args.get("one_cycle_total_steps", ONE_CYCLE_TOTAL_STEPS)
+
+        self.train_acc = Accuracy()
+        self.val_acc = Accuracy()
+        self.test_acc = Accuracy()
+
+    @staticmethod
+    def add_to_argparse(parser):
+        parser.add_argument(
+            "--optimizer", type=str, default=OPTIMIZER, help="optimizer class from torch.optim"
+        )
+        parser.add_argument("--lr", type=float, default=LR)
+        parser.add_argument("--one_cycle_max_lr", type=float, default=None)
+        parser.add_argument(
+            "--loss", type=str, default=LOSS, help="loss function from torch.nn.functional"
+        )
+
+        return parser
+
+    def contigure_optimizer(self):
+        """
+        설정 값으로 optimizer를 구성하여 반환
+
+        returns
+        ----------
+        if onecycle learning rate
+            dict{optimizer, learnrate schduler, monitor}
+                monitor : 감독할 loss 종류 (str)
+        else
+            optimizer
+        """
+        optimizer = self.optimzer_class(self.parameters(), lr=self.lr)
+        if self.one_cycle_max_lr is None:
+            # one_cycle 정책을 사용하지 않는다면 고정된 lr을 갖는 optimizer 반환
+            return optimizer
+        
+        schedular = torch.optim.lr_scheduler.OnecycleLR( 
+
+        optimizer=optimizer,
+        max_lr=self.one_cycle_max_lr,
+        total_steps=self.one_cycle_total_steps,
+        )
+
+        return {"optimizer": optimizer, "lr_scheduler": schedular, "monitor": "val_loss"}
+
+
+    def forward():
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+        self.log("train_loss", loss)
+        self.train_acc(logits, y)
+        self.log("train_acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+        self.log("val_loss", loss, prog_bar=True)
+        self.val_acc(logits, y)
+        self.log("val_acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        self.test_acc(logits, y)
+        self.log("test_acc", self.test_acc, on_steps=False, on_epoch=True)
+
+
+
+
+
+
+
+
+
