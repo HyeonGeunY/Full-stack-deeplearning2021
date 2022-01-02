8c0e005834b079597f3876d5470bb9beeb4f5430
HyeonGeunY <hgyoon0928@gmail.com>
2022-01-02 13:09:22 +0000

    22.01.02

--- LABs/lab1/text_recognizer/data/__init__.py
+++ LABs/lab1/text_recognizer/data/__init__.py
@@ -1,3 +1,9 @@
 from .util import BaseDataset
 from .base_data_module import BaseDataModule
 from .mnist import MNIST
+
+# __iit__.py 파일을 만들면서 해당 디렉토리를 패키지로 만든다.
+# __init__.py 파일에 from .util import BaseDataset과 같은 구문을 넣음으로써 중간 모듈 이름을 생략할 수 있다.
+# ex)
+# import data.util.BaseDatset -> import data
+# data.util.BaseDataset -> data.BaseDataset
